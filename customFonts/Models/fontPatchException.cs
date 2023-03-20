afd6440e3c767bc3212d4daa3f786211d8c629ca
jbcarreon123 <jbcarreon212@gmail.com>
2023-03-20 14:53:17 +0800

    Add project files.

---
+++ customFonts/Models/fontPatchException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace customFonts.Models
+{
+    class fontPatchException : Exception
+    {
+        public fontPatchException() : base () { }
+        public fontPatchException(string message) : base(message) { }
+        public fontPatchException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
